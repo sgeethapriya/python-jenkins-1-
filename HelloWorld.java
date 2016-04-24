bd754484a29a7d99d286718cd10e434a122b1de7
yanleio <leio@live.com>
2016-04-24 18:50:41 +0800

    Update HelloWorld.java
    just changed a bit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld{ 
 
    public static void main(String[] args){ 
-       System.out.println( "Hello World! \n" );  
+       System.out.println( "Hello World! \n" ); 
+       System.out.println( "MAY I GIVE A shit! \n" ); 
+       
    } 
 } 
