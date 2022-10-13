90e5cefda0a6ce392efc9023b564bbb450c3701f
yanleio <yanleio@126.com>
2022-10-13 15:22:47 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 
    public static void main(String[] args){ 
        System.out.println( "Hello World! \n" ); 
-       System.out.println( "MAY I GIVE A shit! \n" ); 
+      
        
    } 
 } 
